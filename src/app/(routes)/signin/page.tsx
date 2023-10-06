66da83ee76a078c15309e7e9262f22a772358f02
UltimateProdigy <ayobamiakinola84@gmail.com>
2023-10-06 14:44:51 +0100

    Login Auth with Local Storage Done

--- src/app/(routes)/signin/page.tsx
+++ src/app/(routes)/signin/page.tsx
@@ -1,31 +1,48 @@
-'use client'
+'use client';
 
-import Password from "antd/es/input/Password";
+import { Input } from 'antd'; // Import Ant Design Input component
 import "./signin.css";
 import Link from "next/link";
-import { FormEvent, useRef } from "react";
+import { FormEvent, useState } from "react"; // Import useState instead of useRef for form fields
 import { useRouter } from "next/navigation";
 
 const Signin: React.FC = () => {
-  const usernameRef = useRef<HTMLInputElement>(null);
-  const passwordRef = useRef<HTMLInputElement>(null);
-  const router = useRouter()
+  const [loginUsername, setLoginUsername] = useState<string>("");
+  const [loginPassword, setLoginPassword] = useState<string>("");
+  const router = useRouter();
 
-  const loginUsername = usernameRef.current?.value
-
-  const username = localStorage.getItem('Username')
-  const password = localStorage.getItem('Password')
+  const username = localStorage.getItem("Username") || ""; // Retrieve username from localStorage
+  const password = localStorage.getItem("Password") || ""; // Retrieve password from localStorage
 
   const handleLogin = (e: FormEvent) => {
-    e.preventDefault()
-  }
+    e.preventDefault();
+    
+    // Check if entered username and password match the stored values
+    if (username === loginUsername && password === loginPassword) {
+      alert("Signed In Successfully");
+      router.push("/dashboard");
+    } else {
+      alert("Invalid Credentials, Please Try Again!!!");
+    }
+  };
 
   return (
     <div>
       <h3 className="login-intro">Please Log In</h3>
       <form className="signin-form" onSubmit={handleLogin}>
-        <input type="text" ref={usernameRef} placeholder="Username" required />
-        <input type="password" ref={passwordRef} placeholder="Password" required />
+        <Input
+          type="text"
+          placeholder="Username"
+          value={loginUsername}
+          onChange={(e) => setLoginUsername(e.target.value)}
+          required
+        />
+        <Input.Password
+          placeholder="Password"
+          value={loginPassword}
+          onChange={(e) => setLoginPassword(e.target.value)}
+          required
+        />
         <button type="submit" className="login-btn">
           Sign In
         </button>
